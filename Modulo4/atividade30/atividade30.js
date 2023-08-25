9a55ec68c0806e7522d81d6b8e72a748821e2c75
Walter Andrade <65465090+walterwa@users.noreply.github.com>
2023-08-25 07:56:34 -0300

    finalização atividade 30

--- Modulo4/atividade30/atividade30.js
+++ Modulo4/atividade30/atividade30.js
@@ -9,6 +9,11 @@ por exemplo: um programa de calculadora*/
 
 const prompt = require('prompt-sync')();
 
+//Apresentação da calculadora
+const apresentação = () => `Para usar a calculadora você precisa digitar 2 números (um por vez) e um operador [+ - / *]`
+
+console.log(apresentação())
+
 //função para aceitar apenas números no prompt
 function promptInt(mensagem, tenteNovamente) {
     let msg = mensagem; 
@@ -19,17 +24,24 @@ function promptInt(mensagem, tenteNovamente) {
     }
 }
 
-
-function receberValores() {
+//função para receber dados e fazer os calculos
+function calculadora() {
     let numero1 = promptInt('Número 1: ', "Por favor, digite um número.Tente outra vez: ")
-    //console.log(`Você digitou: ${numero1}`)
     let numero2 = promptInt('Número 2: ', "Por favor, digite um número.Tente outra vez: ")
-    //console.log(`Você digitou: ${numero2}`)
     let operador = prompt('Digite o operador desejado [+ - / *]: ')
-
-    return `Você digitou os números: ${numero1} ${operador} ${numero2}`
+    console.log(`\nResultado: `)
+    if(operador == '+') {
+        return numero1 + numero2
+    } else if (operador == '-') {
+        return numero1 - numero2
+    } else if (operador == '/') {
+        return numero1 / numero2
+    } else if (operador == '*') {
+        return numero1 * numero2
+    } else {
+        return `Operador ${operador} inválido.`
+    }
 }
 
-console.log(receberValores())
+console.log(calculadora())
 
-console.log(`Teste Calculadora: `)
