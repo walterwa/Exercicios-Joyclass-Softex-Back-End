307d530330cad4a854c6c7497901faa3e7a47eb4
Walter Andrade <65465090+walterwa@users.noreply.github.com>
2023-10-24 09:46:16 -0300

    projeto hotel DB

---
+++ exerciciosSalaDeAula/bancoDeDados/hotel/hotel.sql
@@ -0,0 +1,10 @@
+#inserindo no banco de dados
+
+#Insert 
+#INSERT INTO hotel.funcionario(nome, cargo) VALUES ('Paulo', 'Gerente');
+
+#Deleter funcionário 2 que eu add repetido
+#DELETE FROM hotel.funcionario WHERE idfuncionario = 2;
+
+#Selecionando todos os dados
+SELECT * FROM hotel.funcionario;
---
+++ exerciciosSalaDeAula/bancoDeDados/hotel/server.ts
@@ -0,0 +1,99 @@
+import express, { json } from "express";
+import mysql from "mysql2";
+
+
+const conexao = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: //add senha ao rodar o código
+    database: "hotel"
+})
+
+const app = express();
+
+app.use(json());
+
+//Consulta no Banco de Dados via BackEnd.
+app.get('/funcionario', (req, res) => {
+    
+    const consulta = "SELECT funcionario.nome, funcionario.cargo FROM hotel.funcionario;";
+
+    conexao.query(consulta, (err, resultado) => {
+        if(err){
+            console.log(err);
+            res.status(404).json({'erro': err})
+        } else {
+            res.status(200).json(resultado)
+        }
+    })
+})
+
+//Inserir dados com o POST
+
+app.post('/cadastrar', (req, res) => {
+    
+    const dados = req.body;
+    const sql = "INSERT INTO hotel.funcionario SET ?;"
+    conexao.query(sql, dados, (err, resultado) => {
+        if(err) {
+            console.log(err)
+            res.status(400).json({'erro': err});
+        } else {
+            res.status(201).json(resultado);
+        }
+    })
+})
+
+//SELECT via ID (Chave primária)
+app.get('/funcionario/:id', (req, res) => {
+
+    const id = req.params.id;
+    const sql = "SELECT * FROM hotel.funcionario WHERE idfuncionario =?;"
+    conexao.query(sql,id, (err, resultado) => {
+        if(err) {
+            console.log(err);
+            res.status(404).json({'erro': err})
+        } else {
+            res.status(200).json(resultado);
+        }
+    })
+})
+
+//Alterando o funcionário
+
+app.put('/alterar/:id', (req, res) => {
+
+    const id = req.params.id;
+    const dados = req.body;
+    const sql = "UPDATE funcionario SET ? WHERE idfuncionario =?;"
+    conexao.query(sql,[dados,id], (err, resultado) => {
+        if(err) {
+            console.log(err); //não localizado
+            res.status(404).json({'erro': err});
+        } else {
+            res.status(200).json(resultado);
+        }
+    })
+});
+
+//DELETE
+//buscar pelo índice e deletar
+
+app.delete('/excluir/:id', (req, res) => {
+
+    const id = req.params.id;
+    const sql = "DELETE FROM hotel.funcionario WHERE idfuncionario=?;"
+    conexao.query(sql, id, (err, resultado) => {
+        if(err) {
+            console.log(err);
+            res.status(404).json({'erro': err});
+        } else {
+            res.status(201).json(resultado)
+        }
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Funcionou!');
+})
